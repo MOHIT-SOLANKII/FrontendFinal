69698ffc55fbb256828a201eca73fe66910b531d
MOHIT-SOLANKII <mohitslnki773@gmail.com>
2024-11-19 00:43:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'; // Optional, for styling
+import Form from './components/Form'; // Adjust path if necessary
+
+function App() {
+  return (
+    <div className="">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
