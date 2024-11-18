636d4979fd83398f8427ef602e39084ace43509d
MOHIT-SOLANKII <mohitslnki773@gmail.com>
2024-11-19 01:35:47 +0530

    5

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -189,7 +189,7 @@ const Form = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/upload",
+        "https://nptel-backend.onrender.com/upload",
         formData,
         {
           headers: { "Content-Type": "multipart/form-data" },
